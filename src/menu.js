661c950a2ab8add502a0df4e12a3671fb48d14bc
Chloe Urisohn <chloeurisohn@gmail.com>
2021-05-31 12:14:21 +0100

    add menu class

---
+++ src/menu.js
@@ -0,0 +1,13 @@
+class Menu {
+    constructor(title, price, description) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+    }; 
+    sayHi() {
+        console.log('This is the menu class')
+    }
+
+};
+
+export default Menu
--- src/navbar.js
+++ src/navbar.js
@@ -1,4 +1,5 @@
 import home from './home'
+import Menu from './menu'
 
 const navbar = () => {
     const addNavbar = () => {
@@ -13,11 +14,22 @@ const navbar = () => {
         const heading = '<h1>PIZZAFACE</h1>';
         body.insertAdjacentHTML("afterbegin", navElement);
         setHomeActive();
+        setEventListeners();
+    }
+    const setEventListeners = () => {
+        const home = document.getElementById('home');
+        const menu = document.getElementById('menu');
+        const about = document.getElementById('about');
     }
     const setHomeActive = () => {
         const home = document.getElementById('home');
         home.setAttribute('style', 'text-decoration: underline');
         navHome();
+        setMenuActive();
+    }
+    const setMenuActive = () => {
+        const menu = new Menu();
+        menu.sayHi();
     }
     const navHome = () => {
         const homePage = home();
