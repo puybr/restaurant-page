5f0c4a44342bebe9fffbde0cff1a2047de81984f
Chloe Urisohn <56723522+ghost-goblin@users.noreply.github.com>
2024-11-17 18:07:41 +0200

    fix bugs

--- src/home.js
+++ src/home.js
@@ -4,9 +4,21 @@ const home = () => {
     const renderHome = () => {
         const home = `
         <div class="home">
-        <h1>PIZZAFACE</h1>
-        <h3>FACEBOOK | TWITTER | ORDER ONLINE</h3>
-        <img src="images/1.jpg" alt="home" width="100%">
+            <span class="splash">
+                <h1>PIZZAFACE</h1>
+                <ul class="subtext">
+                <span>
+                        <li id="facebook">FACEBOOK</li>
+                    </span>
+                    <span>
+                        <li id="twitter">TWITTER</li>
+                    </span>
+                    <span>
+                        <li id="order">ORDER ONLINE</li>
+                    </span>
+                </ul>
+            </span>
+            <img src="images/1.jpg" alt="home" width="100%">
         </div>`;
         const body = document.querySelector('body');
         body.insertAdjacentHTML('beforeend', home);  
